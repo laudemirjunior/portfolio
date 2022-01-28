b3f5446671af263fe230b62e0e4704365aa63486
Laudemir <nlaudemir@gmail.com>
2022-01-27 21:11:23 -0300

    fix: margin top contact

--- src/components/contact/styles.js
+++ src/components/contact/styles.js
@@ -5,7 +5,6 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 50px;
   form {
     width: 100%;
     max-width: 1500px;
